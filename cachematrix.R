0e54fe8015e360e47af41e68fb7e5eec3a063b97
evgraph <vk@eblans.org>
2015-01-26 00:50:53 +0600

    first

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First function creates matrix avaliable to cache oneself.
+## Second function calculates the inverse matrix and caches 
+## the result.
 
-## Write a short comment describing this function
+## Function creates matrix avaliable to cache oneself.
+## Function takes as argument matrix 'x', by default 'x' 
+## is empty matrix.
+## Return the list of functions for setting and getting
+## matrix, setting and getting inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## Variable for caching inverse matrix
+    inverse <- NULL
+    
+    ## Functions takes matrix 'y' as argument that by
+    ## default is empty matrix, and assign it to 'x',
+    ## and update cached value inverse.
+    set <- function(y = matrix()) {
+        x <<- y
+        inverse <<- NULL
+    }
+    
+    ## Function returns matrix 'x'.
+    get <- function() {
+        x
+    }
+    
+    ## Function updates cached value inverse.
+    ## Function takes matrix 'm' as an argument.
+    setinverse <- function (m) {
+        inverse <- m
+    }
+    
+    ## Function returns cache value of inversed 
+    ## matrix 'x'.
+    getinverse <- function() {
+        inverse
+    }
+    
+    list( set = set, get = get, 
+          setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Function returns inverse of 'x'.
 
 cacheSolve <- function(x, ...) {
+    inverse <- x$getinverse()
+    
+    if (!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse) 
+    }
+    
+    matrix <- x$get()
+    inverse <- solve(matrix)
+    x$setinverse(inverse)
+    inverse
         ## Return a matrix that is the inverse of 'x'
 }
